bea8ce47b2b692f8c2b3f0742622c4619bb823a6
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-17 22:53:10 -0700

    add seeds.rb skeleton

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create!([
+  { title: "Cannabinoid receptor agonists are mitochondrial inhibitors: a unified hypothesis of how cannabinoids modulate mitochondrial function and induce cell death",
+    authors: ["Andriani Athanasiou", "Anna B. Clarke", "Amy E. Turner", "Nethia M. Kumaran",
+              "Sara Vakilpour", "Paul A. Smith", "Dimitra Bagiokou", "Tracey D. Bradshaw",
+              "Andrew D. Westwell", "Lin Fang", "Dileep N. Lobo", "Cris S. Constantinescu",
+              "Vittorio Calabrese", "Andrzej Loesch", "Stephen P.H. Alexander",
+              "Richard H. Clothier", "David A. Kendall", "Timothy E. Bates"],
+    journal: "Biochemical and Biophysical Research Communications",
+    volume: "364",
+    issue: "1",
+    year_published: "2007",
+    month_published: "December",
+    article_url: "http://www.researchgate.net/profile/Andrzej_Loesch/publication/5912829_Cannabinoid_receptor_agonists_are_mitochondrial_inhibitors_a_unified_hypothesis_of_how_cannabinoids_modulate_mitochondrial_function_and_induce_cell_death/links/53d277350cf228d363e942f8.pdf",
+    dosage_included: true,
+    dosage_amt: "",
+    disease_tags: ["cancer", "lung cancer"],
+    phyto_cannabinoid: ["CBG", "Cannabigerol","CBD", "Cannabidiol", "CBC", "Cannabichromene","THC", "Tetrahydrocannabinol","THVC", "Tretrahydrocannabivarin"],
+    keywords: ["Cannabinoid", "Apoptosis", "Mitochondria", "Anandamide", "D-9-Tetrahydrocannabinol", "HU 210"]
+  },
+  { title: ,
+    authors: ,
+    journal: ,
+    volume: ,
+    issue: ,
+    year_published: ,
+    month_published: ,
+    article_url: ,
+    dosage_included: ,
+    dosage_amt: ,
+    disease_tags: ,
+    phyto_cannabinoid: ,
+    keywords:
+  }
+])
