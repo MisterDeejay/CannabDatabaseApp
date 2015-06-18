b138da18580336c21465513f60d034baa10c37ce
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-18 01:56:19 -0700

    finish articles migration

--- db/migrate/20150618055801_create_articles.rb
+++ db/migrate/20150618055801_create_articles.rb
@@ -1,6 +1,16 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
+			t.text :title, null: false
+			t.integer :journal_id
+			t.string :volume
+			t.string :issue
+			t.date :date_published
+			t.text :article_url
+			t.boolean :dosage_included
+			t.string :dosage_amt
+
+			t.timestamps null: false
     end
   end
 end
