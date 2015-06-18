1f97ff1c03c16f4f3d120772e80c784698b97984
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-18 01:44:02 -0700

    finish users migration

---
+++ db/migrate/20150618055546_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+			t.string :full_name, null: false
+			t.string :email, null: false
+			t.string :username, null: false
+			t.string :session_token, null: false
+			t.string :password_digest, null: false
+
+			t.timestamps null: false
+    end
+
+		add_index :users, :username, unique: true
+		add_index :users, :email, unique: true
+  end
+end
