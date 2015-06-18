24168d32fa8749292f38acbdf14c5fe9840cc7e2
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-18 13:32:11 -0700

    create keywords migration

---
+++ db/migrate/20150618202413_create_keywords.rb
@@ -0,0 +1,10 @@
+class CreateKeywords < ActiveRecord::Migration
+  def change
+    create_table :keywords do |t|
+			t.string :name, null: false
+			t.references :taggable, polymorphic: true, index: true
+
+			t.timestamps null: false
+    end
+  end
+end
