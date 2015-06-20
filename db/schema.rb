a9bde7b0c5d4caced075062da1d35d592c27d1d6
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-20 13:21:24 -0700

    add tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150618203851) do
+ActiveRecord::Schema.define(version: 20150618221812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,7 +47,9 @@ ActiveRecord::Schema.define(version: 20150618203851) do
   add_index "disease_tags", ["taggable_type", "taggable_id"], name: "index_disease_tags_on_taggable_type_and_taggable_id", using: :btree
 
   create_table "journals", force: :cascade do |t|
-    t.string "name", null: false
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "keywords", force: :cascade do |t|
