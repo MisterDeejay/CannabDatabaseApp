4f1cb7d078d9e51df8e738419e1ccde9e34f9332
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-20 13:20:21 -0700

    add taggable concern

---
+++ app/models/concerns/taggable.rb
@@ -0,0 +1,12 @@
+module Taggable
+	extend ActiveRecord::Concern
+
+	included do
+		has_many :taggings, as: :taggable, dependent: :destroy
+		has_many :tags, through: :taggable
+	end
+
+	def tag_names
+		tags.map(&:name)
+	end
+end
