54a010548c95322ea9c4803010479badb4fa0b93
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-22 16:17:26 -0700

    add tagging model

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,7 @@
 class Tag < ActiveRecord::Base
+  has_many :taggings
+
+  has_many :keywords, through: :taggings, source: :taggable, source_type: Keyword
+	has_many :phyto_cannabinoids, through: :taggings, source: :taggable, source_type: PhytoCannabinoid
+	has_many :disease_tags, through: :taggings, source: :taggable, source_type: DiseaseTag
 end
