5da9ec8680bccedad478f999a03c9e80a2b09f11
MisterDeejay <khaaliqdejan@gmail.com>
2015-06-26 14:29:21 -0700

    add signup, confirmation and thank_you

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
+	get '/thank-you', to: 'static_pages#thank-you'	
+	get '/confirmation', to: 'static_pages#confirmation'
+	post '/signup', to: 'users#email_signup'
 end
